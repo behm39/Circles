090aaea102a6d16efc49a682707812c9ffc80442
behm39 <behm.39@buckeyemail.osu.edu>
2017-01-29 17:30:08 -0500

    Update Vector.java

--- Circles/src/Vector.java
+++ Circles/src/Vector.java
@@ -1,74 +1,81 @@
-package circles;
 
 public class Vector {
-	private double x;
-	private double y;
-	
-	public Vector(){
-		x = 0;
-		y = 0;
-	}
-	
-	public Vector(double x, double y){
-		this.x = x;
-		this.y = y;
-	}
+    private double x;
+    private double y;
 
-	public Vector(double t){
-		x = Math.cos(t);
-		y = Math.sin(t);
-	}
-	
-	public void subtract(Vector v){
-		x -= v.getX();
-		y -= v.getY();
-	}
-	
-	public void add(Vector v){
-		x += v.getX();
-		y += v.getY();
-	}
-	
-	public void scale(double s){
-		x *= s;
-		y *= s;
-	}
-	
-	public void setX(double x){
-		this.x = x;
-	}
-	
-	public void setY(double y){
-		this.y = y;
-	}
-	
-	public void setMag(double m){
-		double theta = this.getTheta();
-		this.x = Math.cos(theta);
-		this.y = Math.sin(theta);
-		this.scale(m);
-	}
-	
-	public void setTheta(double t){
-		double mag = this.getMag();
-		x = Math.cos(t);
-		y = Math.sin(t);
-		this.setMag(mag);
-	}
-	
-	public double getTheta(){
-		return Math.atan(y/x);
-	}
-	
-	public double getMag(){
-		return Math.sqrt(x*x + y*y);
-	}
-	
-	public double getX(){
-		return x;
-	}
-	
-	public double getY(){
-		return y;
-	}
+    public Vector() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector(double t) {
+        this.x = Math.cos(t);
+        this.y = Math.sin(t);
+    }
+
+    public double getDistance(Vector v) {
+
+        double deltaX = this.x - v.getX();
+        double deltaY = this.y - v.getY();
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    }
+
+    public void subtract(Vector v) {
+        this.x -= v.getX();
+        this.y -= v.getY();
+    }
+
+    public void add(Vector v) {
+        this.x += v.getX();
+        this.y += v.getY();
+    }
+
+    public void scale(double s) {
+        this.x *= s;
+        this.y *= s;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setMag(double m) {
+        double theta = this.getTheta();
+        this.x = Math.cos(theta);
+        this.y = Math.sin(theta);
+        this.scale(m);
+    }
+
+    public void setTheta(double t) {
+        double mag = this.getMag();
+        this.x = Math.cos(t);
+        this.y = Math.sin(t);
+        this.setMag(mag);
+    }
+
+    public double getTheta() {
+        return Math.atan(this.y / this.x);
+    }
+
+    public double getMag() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
 }
