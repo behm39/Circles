462696d23dc0758fd689e4f52ac249a0b0b4e983
behm39 <behm.39@buckeyemail.osu.edu>
2017-01-29 17:18:52 -0500

    Update Circle.java

--- Circles/src/Circle.java
+++ Circles/src/Circle.java
@@ -1,20 +1,45 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.util.Random;
 
 public class Circle {
 
+    private Main main;
     private Vector pos, vel, acc;
+    private int r;
 
-    public Circle() {
-        this.pos = new Vector();
+    public Circle(Main main) {
+        this.main = main;
+
+        Random rand = new Random();
+
+        this.pos = new Vector(rand.nextInt(main.getWidth()),
+                rand.nextInt(main.getHeight()));
         this.vel = new Vector();
         this.acc = new Vector();
+        this.r = rand.nextInt(25) + 25;
     }
 
     public void tick() {
 
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+
+        this.acc.scale(0);
+
     }
 
     public void render(Graphics2D g2d) {
+
+        AffineTransform old = g2d.getTransform();
+
+        g2d.translate(this.pos.getX(), this.pos.getY());
+
+        g2d.setColor(Color.WHITE);
+        g2d.drawOval(-this.r, -this.r, 2 * this.r, 2 * this.r);
+
+        g2d.setTransform(old);
 
     }
 
