72130d5a1c745059f7e2cb014275f9552be5e09b
behm39 <behm.39@buckeyemail.osu.edu>
2017-01-29 01:06:41 -0500

    Update Circle.java

--- Circles/src/Circle.java
+++ Circles/src/Circle.java
@@ -1,4 +1,21 @@
+import java.awt.Graphics2D;
 
 public class Circle {
+
+    private Vector pos, vel, acc;
+
+    public Circle() {
+        this.pos = new Vector();
+        this.vel = new Vector();
+        this.acc = new Vector();
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics2D g2d) {
+
+    }
 
 }
