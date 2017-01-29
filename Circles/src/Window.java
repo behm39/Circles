4d7e581677498aa8f047136172908237f6a83e10
behm39 <behm.39@buckeyemail.osu.edu>
2017-01-29 00:58:30 -0500

    Add files via upload

--- Circles/src/Window.java
+++ Circles/src/Window.java
@@ -1,4 +1,24 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Window {
+
+    public Window(Main main, String title, int width, int height) {
+
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.add(main);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+
+    }
 
 }
